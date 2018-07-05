9857eeb1d434728288709e31367cf43e5b429ef8
Sok Khoinsreng <sokkhoinsreng@Soks-MacBook-Pro.local>
2018-07-05 16:47:14 +0700

    add-color-global

---
+++ src/component/color/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    COLORS: require('./colorStyle.js'),
+};
---
+++ src/component/color/primaryColors.js
@@ -0,0 +1,11 @@
+COLORS = {
+    primaryRed: 'rgb(249,64,90)',
+    primaryGreen: 'rgb(129,206,66)',
+    primaryWhite: 'rgb(254,254,254)',
+    primaryGray: 'rgb(167,167,167)',
+    primaryBlack: 'rgb(57,57,57)',
+    primaryTapIcon: 'rgb(86,86,86)',
+    primaryBackgroundColor: 'rgb(244,244,244)',
+}
+
+module.exports = COLORS;
